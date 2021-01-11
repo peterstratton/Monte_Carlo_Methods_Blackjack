29b67ba76953b603d70de6fd706c364fe510ada8
Peter <pstratto@ucsd.edu>
2021-01-11 15:33:48 -0800

    changes

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import gym
 import numpy as np
+from utils import evaluate_policy
 from on_policy import on_policy_first_visit_control
+
+RUNS = 10000 # how many times to test policy
+GAMMA = 1 # discount factor
+EPS = 0.3 # e value
 
 if __name__ == "__main__":
     # setup gym environment
     env = gym.make('Blackjack-v0')
     env.reset()
 
-    # get necessary environment variables
-    # num_states = env.nS
-    # num_actions = env.nA
-    # state_transition = env.P
-    #
-    # print(num_states)
-    # print(num_actions)
-    # print(state_transition)
+    policy = on_policy_first_visit_control(env, gamma=GAMMA, eps=EPS)
+    print("Optimal policy: \n" + str(policy))
+
+    wins, losses, scores = evaluate_policy(env, policy, runs=RUNS)
+    # display results
+    print("-------------------------------------------------------------------")
+    print("Policy resulted in winning: " + str((wins / float(RUNS)) * 100) + \
+          " percent of the time")
+    print("Policy resulted in losing: " + str((losses / float(RUNS)) * 100) + \
+          " percent of the time")
+    print("-------------------------------------------------------------------")
+    env.close()
